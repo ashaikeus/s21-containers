cb5aff5a82aa832d1181d0519cd502db535993e2
vyperpunk <suloversanja@gmail.com>
2024-03-02 18:18:31 +0300

    finished set, added tests for all functions, applied style

--- src/s21_set.h
+++ src/s21_set.h
@@ -2,325 +2,385 @@
 #define S21_SET_H
 
 #include <iostream>
+#include <limits>
 
+namespace s21 {
 template <class T>
 class Set {
-   public:
-    using key_type = T;
-    using value_type = T;
-    using reference = value_type &;
-    using const_reference = const value_type &;
-    using size_type = size_t;
+ public:
+  using key_type = T;
+  using value_type = T;
+  using reference = value_type&;
+  using const_reference = const value_type&;
+  using size_type = size_t;
 
-   protected:
-    struct Node {
-        Node* left_ = nullptr;
-        Node* right_ = nullptr;
-        Node* parent_ = nullptr;
-        value_type data_;
+ protected:
+  struct Node {
+    Node* left_ = nullptr;
+    Node* right_ = nullptr;
+    Node* parent_ = nullptr;
+    value_type data_;
 
-        Node(const value_type data) : data_(data) {};
-        ~Node() {
-            if (left_ != nullptr) {
-                delete left_;
-                left_ = nullptr;
-            }
-            if (right_ != nullptr) {
-                delete right_;
-                right_ = nullptr;
-            }
-        }
-    };
+    Node(const value_type data) : data_(data){};
+    ~Node() {
+      if (left_ != nullptr) {
+        delete left_;
+        left_ = nullptr;
+      }
+      if (right_ != nullptr) {
+        delete right_;
+        right_ = nullptr;
+      }
+    }
+  };
 
+ private:
+  Node* root_;
+  size_type nodeCount_;
+
+ public:
+  template <class value_type>
+  class Iterator {
    private:
+    Node* current_;
+    Node* successor_;
     Node* root_;
-    size_type nodeCount_;
 
    public:
-    template <class value_type>
-    class Iterator {
-       private:
-        Node* current_;
-        Node* successor_;
-        Node* root_;
+    Iterator(Node* node) : current_(node), successor_(nullptr), root_(root_) {}
 
-       public:
-        Iterator(Node* node) : current_(node), successor_(nullptr), root_(root_) {}
-
-        Iterator operator++() {
-            if (current_ == nullptr) {
-                current_ = root_;
-                if (current_ == nullptr) throw std::underflow_error("Empty tree");
-                while (current_->left_ != nullptr) {
-                    current_ = current_->left_;
-                }
-            }
-            if (current_->right_ != nullptr) {
-                current_ = current_->right_;
-                while (current_->left_ != nullptr) {
-                    current_ = current_->left_;
-                }
-            } else {
-                successor_ = current_->parent_;
-                while (current_->parent_ != nullptr && current_ == current_->parent_->right_) {
-                    current_ = successor_;
-                    successor_ = successor_->parent_;
-                }
-                current_ = successor_;
-            }
-            return *this;
+    Iterator operator++() {
+      if (current_ == nullptr) {
+        current_ = root_;
+        if (current_ == nullptr) throw std::underflow_error("Empty tree");
+        while (current_->left_ != nullptr) {
+          current_ = current_->left_;
         }
-
-        template <class L>
-        bool operator!=(const L& other) const {
-            return (current_ != other.getCurrent());
+      }
+      if (current_->right_ != nullptr) {
+        current_ = current_->right_;
+        while (current_->left_ != nullptr) {
+          current_ = current_->left_;
         }
-
-        value_type operator*() const {
-            return current_->data_;
+      } else {
+        successor_ = current_->parent_;
+        while (current_->parent_ != nullptr &&
+               current_ == current_->parent_->right_) {
+          current_ = successor_;
+          successor_ = successor_->parent_;
         }
-
-        Node* getCurrent() const {
-            return current_;
-        }
-    };
-
-    template <class value_type>
-    class ConstIterator {
-       private:
-        Node* current_;
-        Node* successor_;
-        Node* root_;
-
-       public:
-        ConstIterator(Node* node) : current_(node), successor_(nullptr), root_(root_) {}
-        
-        ConstIterator operator++() {
-            if (current_ == nullptr) {
-                current_ = root_;
-                if (current_ == nullptr) throw std::underflow_error("Empty tree");
-                while (current_->left_ != nullptr) {
-                    current_ = current_->left_;
-                }
-            }
-            if (current_->right_ != nullptr) {
-                current_ = current_->right_;
-                while (current_->left_ != nullptr) {
-                    current_ = current_->left_;
-                }
-            } else {
-                successor_ = current_->parent_;
-                while (current_->parent_ != nullptr && current_ == current_->parent_->right_) {
-                    current_ = successor_;
-                    successor_ = successor_->parent_;
-                }
-                current_ = successor_;
-            }
-            return *this;
-        }
-
-        template <class L>
-        bool operator!=(const L& other) const {
-            return (current_ != other.getCurrent());
-        }
-
-        value_type operator*() const {
-            return current_->data_;
-        }
-
-        Node* getCurrent() const {
-            return current_;
-        }
-    };
-
-    using iterator = Iterator<T>;
-    using const_iterator = ConstIterator<T>;
-
-    Set() : root_(nullptr), nodeCount_(0) {}
-
-    Set(std::initializer_list<value_type> const &items) : root_(nullptr), nodeCount_(0) {
-        for (auto i : items) {
-            insert(i);
-        }
+        current_ = successor_;
+      }
+      return *this;
     }
 
-    Set(const Set &copySet) : root_(nullptr), nodeCount_(0) {
-        if (copySet.root_ != nullptr) {
-            root_ = new Node(copySet.root_->data_);
-            nodeCount_++;
-            copyNodes(copySet.root_, root_);
+    template <class L>
+    bool operator!=(const L& other) const {
+      return (current_ != other.getCurrent());
+    }
+
+    value_type operator*() const {
+      if (current_ == nullptr)
+        throw std::invalid_argument("Null pointer dereference");
+      return current_->data_;
+    }
+
+    Node* getCurrent() const { return current_; }
+  };
+
+  template <class value_type>
+  class ConstIterator {
+   private:
+    Node* current_;
+    Node* successor_;
+    Node* root_;
+
+   public:
+    ConstIterator(Node* node)
+        : current_(node), successor_(nullptr), root_(root_) {}
+
+    ConstIterator operator++() {
+      if (current_ == nullptr) {
+        current_ = root_;
+        if (current_ == nullptr) throw std::underflow_error("Empty tree");
+        while (current_->left_ != nullptr) {
+          current_ = current_->left_;
         }
-    }
-
-    void copyNodes(Node* source, Node* destination) {
-        if (source->left_ != nullptr) {
-            destination->left_ = new Node(source->left_->data_);
-            destination->left_->parent_ = destination;
-            nodeCount_++;
-            copyNodes(source->left_, destination->left_);
+      }
+      if (current_->right_ != nullptr) {
+        current_ = current_->right_;
+        while (current_->left_ != nullptr) {
+          current_ = current_->left_;
         }
-        if (source->right_ != nullptr) {
-            destination->right_ = new Node(source->right_->data_);
-            destination->right_->parent_ = destination;
-            nodeCount_++;
-            copyNodes(source->right_, destination->right_);
+      } else {
+        successor_ = current_->parent_;
+        while (current_->parent_ != nullptr &&
+               current_ == current_->parent_->right_) {
+          current_ = successor_;
+          successor_ = successor_->parent_;
         }
+        current_ = successor_;
+      }
+      return *this;
     }
 
-    Set(Set &&moveSet) : root_(moveSet.root_), nodeCount_(moveSet.nodeCount_) {
-        moveSet.root_ = nullptr;
-        moveSet.nodeCount_ = 0;
+    template <class L>
+    bool operator!=(const L& other) const {
+      return (current_ != other.getCurrent());
     }
 
-    ~Set() {
-        root_ = nullptr;
-        nodeCount_ = 0;
-    };
-
-    Set& operator=(Set &&s) {
-        // Set ptr = s;
-        // if (this != s) {
-        //     clear();
-        //     root_ = ptr.root_;
-        //     ptr->root_ = nullptr;
-        // }
-        // return *this;
+    value_type operator*() const {
+      if (current_ == nullptr)
+        throw std::invalid_argument("Null pointer dereference");
+      return current_->data_;
     }
 
-    bool operator==(const Set &other) const {
-        bool ret = true;
-        if (size() != other.size()) ret = false;
-        else for (const_iterator it = cbegin(), ot = other.cbegin(); it != cend() && ot != other.cend() && ret;) {
-            if (*it != *ot) ret = false;
-            ++it;
-            ++ot;
-        }
-        return ret;
-    }
+    Node* getCurrent() const { return current_; }
+  };
 
-    iterator begin() {
-        Node* cur = root_;
-        while (cur->left_) {
+  using iterator = Iterator<T>;
+  using const_iterator = ConstIterator<T>;
+
+  Set() : root_(nullptr), nodeCount_(0) {}
+
+  Set(std::initializer_list<value_type> const& items)
+      : root_(nullptr), nodeCount_(0) {
+    for (auto i : items) {
+      insert(i);
+    }
+  }
+
+  Set(const Set& copySet) : root_(nullptr), nodeCount_(0) {
+    if (copySet.root_ != nullptr) {
+      root_ = new Node(copySet.root_->data_);
+      nodeCount_++;
+      copyNodes(copySet.root_, root_);
+    }
+  }
+
+  void copyNodes(Node* source, Node* destination) {
+    if (source->left_ != nullptr) {
+      destination->left_ = new Node(source->left_->data_);
+      destination->left_->parent_ = destination;
+      nodeCount_++;
+      copyNodes(source->left_, destination->left_);
+    }
+    if (source->right_ != nullptr) {
+      destination->right_ = new Node(source->right_->data_);
+      destination->right_->parent_ = destination;
+      nodeCount_++;
+      copyNodes(source->right_, destination->right_);
+    }
+  }
+
+  Set(Set&& moveSet) : root_(moveSet.root_), nodeCount_(moveSet.nodeCount_) {
+    moveSet.root_ = nullptr;
+    moveSet.nodeCount_ = 0;
+  }
+
+  ~Set() {
+    root_ = nullptr;
+    nodeCount_ = 0;
+  };
+
+  Set& operator=(Set&& s) {
+    Set ptr = s;
+    if (this != s) {
+      clear();
+      root_ = ptr.root_;
+      ptr->root_ = nullptr;
+    }
+    return *this;
+  }
+
+  bool operator==(const Set& other) const {
+    bool ret = true;
+    if (size() != other.size())
+      ret = false;
+    else
+      for (const_iterator it = cbegin(), ot = other.cbegin();
+           it != cend() && ot != other.cend() && ret;) {
+        if (*it != *ot) ret = false;
+        ++it;
+        ++ot;
+      }
+    return ret;
+  }
+
+  iterator begin() {
+    Node* cur = root_;
+    if (cur != nullptr)
+      while (cur->left_) {
+        cur = cur->left_;
+      }
+    return cur;
+  }
+
+  const_iterator cbegin() const {
+    Node* cur = root_;
+    if (cur != nullptr)
+      while (cur->left_) {
+        cur = cur->left_;
+      }
+    return cur;
+  }
+
+  iterator end() const {
+    Node* cur = root_;
+    if (cur != nullptr) {
+      while (cur->right_) {
+        cur = cur->right_;
+      }
+      cur = cur->right_;
+    }
+    return cur;
+  }
+
+  const_iterator cend() const {
+    Node* cur = root_;
+    if (cur != nullptr) {
+      while (cur->right_) {
+        cur = cur->right_;
+      }
+      cur = cur->right_;
+    }
+    return cur;
+  }
+
+  bool empty() const { return (nodeCount_ == 0); }
+
+  size_type size() const { return nodeCount_; }
+
+  size_type max_size() {
+    return std::numeric_limits<size_type>::max() / sizeof(value_type);
+  }
+
+  void clear() {
+    if (root_ != nullptr) {
+      delete root_;
+      root_ = nullptr;
+    }
+    nodeCount_ = 0;
+  }
+
+  std::pair<iterator, bool> insert(const value_type& value) {
+    if (nodeCount_ + 1 >= max_size())
+      throw std::range_error("Can't add more elements");
+    bool add = true;
+    bool makeroot = false;
+    Node* newNode = new Node(value);
+    if (root_ == nullptr) {
+      root_ = newNode;
+      makeroot = true;
+    } else {
+      Node* cur = root_;
+      bool looking = true;
+      while (looking) {
+        if (value < cur->data_) {
+          if (cur->left_ == nullptr) {
+            cur->left_ = newNode;
+            newNode->parent_ = cur;
+            looking = false;
+          } else {
             cur = cur->left_;
-        }
-        return cur;
-    }
-
-    const_iterator cbegin() const {
-        Node* cur = root_;
-        while (cur->left_) {
-            cur = cur->left_;
-        }
-        return cur;
-    }
-
-    iterator end() const {
-        Node* cur = root_;
-        while (cur->right_) {
+          }
+        } else if (value > cur->data_) {
+          if (cur->right_ == nullptr) {
+            cur->right_ = newNode;
+            newNode->parent_ = cur;
+            looking = false;
+          } else {
             cur = cur->right_;
-        }
-        return cur->right_;
-    }
-
-    const_iterator cend() const {
-        Node* cur = root_;
-        while (cur->right_) {
-            cur = cur->right_;
-        }
-        return cur->right_;
-    }
-
-    bool empty() const {
-        return (nodeCount_ == 0);
-    }
-
-    size_type size() const {
-        return nodeCount_;
-    }
-
-    size_type max_size() {
-        // return std::numeric_limits<size_t>::max() / sizeof(value_type);
-    }
-
-    void clear() {
-        if (root_ != nullptr) {
-            delete root_;
-            root_ = nullptr;
-            nodeCount_ = 0;
-        }
-    }
-
-    std::pair<iterator, bool> insert(const value_type& value) {
-        bool add = true;
-        bool makeroot = false;
-        Node* newNode = new Node(value);
-        if (root_ == nullptr) {
-            root_ = newNode;
-            makeroot = true;
+          }
         } else {
-            Node* cur = root_;
-            bool looking = true;
-            while (looking) {
-                if (value < cur->data_) {
-                    if (cur->left_ == nullptr) {
-                        cur->left_ = newNode;
-                        newNode->parent_ = cur;
-                        looking = false;
-                    } else {
-                        cur = cur->left_;
-                    }
-                } else if (value > cur->data_) {
-                    if (cur->right_ == nullptr) {
-                        cur->right_ = newNode;
-                        newNode->parent_ = cur;
-                        looking = false;
-                    } else {
-                        cur = cur->right_;
-                    }
-                } else {
-                    add = false;
-                    looking = false;
-                }
-            }
+          add = false;
+          looking = false;
+          delete newNode;
         }
-        if (add) nodeCount_++;
-        if (!add && makeroot) root_ = nullptr;
-        return std::make_pair(newNode, add);
+      }
     }
+    if (add) nodeCount_++;
+    if (!add && makeroot) root_ = nullptr;
+    return std::make_pair(newNode, add);
+  }
 
-    void erase(iterator pos);
-
-    void swap(Set& other) {
-        Node* temp = this->root_;
-        this->root_ = other.root_;
-        other.root_ = temp;
-        int othNodes = other.nodeCount_;
-        other.nodeCount_ = nodeCount_;
-        nodeCount_ = othNodes;
-    }
-
-    void merge(Set& other) {
-        for (iterator it = other.begin(); it != other.end(); ++it) {
-            insert(it->current_.data_);
+  void erase(iterator pos) {
+    if (pos.getCurrent() == root_) {
+      Node* temp = root_;
+      root_ = root_->left_;
+      delete temp;
+      nodeCount_--;
+    } else {
+      Node* cur = root_;
+      while (cur != nullptr) {
+        if (pos.getCurrent() == cur->left_) {
+          Node* temp = cur->left_;
+          cur->left_ = cur->left_->right_;
+          delete temp;
+          nodeCount_--;
+          break;
+        } else if (pos.getCurrent() == cur->right_) {
+          Node* temp = cur->right_;
+          cur->right_ = cur->right_->left_;
+          delete temp;
+          nodeCount_--;
+          break;
         }
+        cur = cur->left_;
+      }
     }
+  }
 
-    // iterator find(const Key& key);
-    // bool contains(const Key& key);
+  void swap(Set& other) {
+    Node* temp = this->root_;
+    this->root_ = other.root_;
+    other.root_ = temp;
+    int othNodes = other.nodeCount_;
+    other.nodeCount_ = nodeCount_;
+    nodeCount_ = othNodes;
+  }
 
-    void printAll() const {
-        // int i = 0;  // limiter in case something breaks
-        if (!empty()) {
-            for (const_iterator it = cbegin(); it.getCurrent() != nullptr && it != cend(); ++it) {
-                std::cout << *it << " ";
-                // i++;
-                // if (i > 40) break;
-            }
-            std::cout << std::endl;
-        } else {
-            std::cout << "No elements in set" << std::endl;
-        }
+  void merge(Set& other) {
+    for (iterator it = other.begin(); it != other.end(); ++it) {
+      insert(it.getCurrent()->data_);
     }
+  }
+
+  iterator find(const value_type& key) {
+    iterator ret = end();
+    for (iterator it = begin(); it != end(); ++it) {
+      if (*it == key) {
+        ret = it;
+        break;
+      }
+    }
+    if (ret.getCurrent() == nullptr) throw std::out_of_range("Can't find key");
+    return ret;
+  }
+
+  bool contains(const value_type& key) {
+    bool ret = false;
+    for (iterator it = begin(); it != end(); ++it) {
+      if (*it == key) {
+        ret = true;
+        break;
+      }
+    }
+    return ret;
+  }
+
+  // void printAll() const {
+  //     if (!empty()) {
+  //         for (const_iterator it = cbegin(); it.getCurrent() != nullptr && it
+  //         != cend(); ++it) {
+  //             std::cout << *it << " ";
+  //         }
+  //         std::cout << std::endl;
+  //     } else {
+  //         std::cout << "No elements in set" << std::endl;
+  //     }
+  // }
 };
+}  // namespace s21
 
 #endif
--- src/test_tree.cpp
+++ src/test_tree.cpp
@@ -1,72 +1,215 @@
-#include "s21_containers.h"
-#include <gtest/gtest.h>
+#include "test.h"
 
-#define EPS 1e-7
-
-int main() {
-  testing::InitGoogleTest();
-  return RUN_ALL_TESTS();
-}
+// int main() {
+//   testing::InitGoogleTest();
+//   return RUN_ALL_TESTS();
+// }
 
 TEST(SetSuite, InitFromList) {
-    std::set<int> trueSet = {50, 45, 1, 30, 49, 46, 55, 51, 53};
-    Set<int> testSet({50, 45, 1, 30, 49, 46, 55, 51, 53});
-    EXPECT_EQ(testSet.size(), trueSet.size());
+  std::set<int> trueSet = {50, 45, 1, 30, 49, 46, 55, 51, 53};
+  s21::Set<int> testSet({50, 45, 1, 30, 49, 46, 55, 51, 53});
+  EXPECT_EQ(testSet.size(), trueSet.size());
+  testSet.clear();
+}
 
-    testSet.clear();
+TEST(SetSuite, InitAssign) {
+  std::set<int> trueSet = {50, 45, 1, 30, 49, 46, 55, 51, 53};
+  s21::Set<int> testSet = s21::Set({50, 45, 1, 30, 49, 46, 55, 51, 53});
+  EXPECT_EQ(testSet.size(), trueSet.size());
+  testSet.clear();
 }
 
 TEST(SetSuite, InitCopy) {
-    Set<int> trueSet({50, 45, 1, 30, 49, 46, 55, 51, 53});
-    Set<int> testSet(trueSet);
-    
-    EXPECT_EQ(testSet.size(), trueSet.size());
-    EXPECT_EQ(1, trueSet == testSet);
+  s21::Set<int> trueSet({50, 45, 1, 30, 49, 46, 55, 51, 53});
+  s21::Set<int> testSet(trueSet);
 
-    trueSet.clear();
-    testSet.clear();
+  EXPECT_EQ(testSet.size(), trueSet.size());
+  EXPECT_TRUE(trueSet == testSet);
+
+  trueSet.clear();
+  testSet.clear();
 }
 
 TEST(SetSuite, InitMove) {
-    Set<int> moveSet({50, 45, 1, 30});
-    Set<int> testSet(std::move(moveSet));
-    Set<int> trueSet({50, 45, 1, 30});
-    EXPECT_EQ(testSet.size(), trueSet.size());
-    EXPECT_EQ(0, moveSet.size());
-    EXPECT_EQ(1, testSet == trueSet);
-    moveSet.clear();
-    testSet.clear();
-    trueSet.clear();
+  s21::Set<int> moveSet({50, 45, 1, 30});
+  s21::Set<int> testSet(std::move(moveSet));
+  s21::Set<int> trueSet({50, 45, 1, 30});
+  EXPECT_EQ(testSet.size(), trueSet.size());
+  EXPECT_EQ(0, moveSet.size());
+  EXPECT_TRUE(testSet == trueSet);
+  moveSet.clear();
+  testSet.clear();
+  trueSet.clear();
 }
 
 TEST(SetSuite, Swap) {
-    Set<int> testSet1({50, 45, 1, 30});
-    Set<int> testSet2({49, 46, 55, 51, 53});
-    Set<int> resSet1({49, 46, 55, 51, 53});
-    Set<int> resSet2({50, 45, 1, 30});
-    testSet1.swap(testSet2);
-    EXPECT_EQ(testSet1.size(), resSet1.size());
-    EXPECT_EQ(testSet2.size(), resSet2.size());
-    EXPECT_EQ(1, testSet1 == resSet1);
-    EXPECT_EQ(1, testSet2 == resSet2);
-    testSet1.clear();
-    testSet2.clear();
-    resSet1.clear();
-    resSet2.clear();
+  s21::Set<int> testSet1({50, 45, 1, 30});
+  s21::Set<int> testSet2({49, 46, 55, 51, 53});
+  s21::Set<int> resSet1({49, 46, 55, 51, 53});
+  s21::Set<int> resSet2({50, 45, 1, 30});
+  testSet1.swap(testSet2);
+  EXPECT_EQ(testSet1.size(), resSet1.size());
+  EXPECT_EQ(testSet2.size(), resSet2.size());
+  EXPECT_TRUE(testSet1 == resSet1);
+  EXPECT_TRUE(testSet2 == resSet2);
+  testSet1.clear();
+  testSet2.clear();
+  resSet1.clear();
+  resSet2.clear();
 }
 
-TEST(SetSuite, SetTests) {
-    std::set<int> trueSet = {50, 45, 1, 30, 49, 46, 55, 51, 53};
-    // for (int num : trueSet) {
-    //     std::cout << num << " ";
-    // }
-    // std::cout << "\n ---- \n";
+TEST(SetSuite, Equal) {
+  s21::Set<int> testSet1({50, 45, 1, 30});
+  s21::Set<int> testSet2({50, 45, 1, 30});
+  s21::Set<int> testSet3({49, 46, 55, 51, 53});
+  EXPECT_TRUE(testSet1 == testSet2);
+  EXPECT_FALSE(testSet1 == testSet3);
+  testSet1.clear();
+  testSet2.clear();
+  testSet3.clear();
+}
 
-    Set<int> testSet({50, 45, 1, 30, 49, 46, 55, 51, 53});
-    // testSet.printAll();
-    // std::cout << *(testSet.begin()) << std::endl;
-    // testSet.printAll();
-    
-    EXPECT_EQ(testSet.size(), trueSet.size());
-    testSet.clear();
+TEST(SetSuite, Begin) {
+  s21::Set<int> testSet({50, 45, 1, 30});
+  std::set<int> trueSet({50, 45, 1, 30});
+  EXPECT_TRUE(*testSet.begin() == *trueSet.begin());
+  EXPECT_TRUE(*testSet.cbegin() == *trueSet.begin());
+  testSet.clear();
+  trueSet.clear();
+}
+
+TEST(SetSuite, End) {
+  s21::Set<int> testSet({50, 45, 1, 30});
+  EXPECT_ANY_THROW(*testSet.end());
+  EXPECT_ANY_THROW(*testSet.cend());
+  testSet.clear();
+}
+
+TEST(SetSuite, Empty) {
+  s21::Set<int> testSet1({50, 45, 1, 30});
+  s21::Set<int> testSet2;
+  EXPECT_FALSE(testSet1.empty());
+  EXPECT_TRUE(testSet2.empty());
+  testSet1.clear();
+  testSet2.clear();
+}
+
+TEST(SetSuite, Size) {
+  s21::Set<int> testSet1({50, 45, 1, 30});
+  std::set<int> trueSet1({50, 45, 1, 30});
+  s21::Set<int> testSet2;
+  EXPECT_EQ(testSet1.size(), trueSet1.size());
+  EXPECT_EQ(testSet2.size(), 0);
+  testSet1.clear();
+  trueSet1.clear();
+  testSet2.clear();
+}
+
+TEST(SetSuite, MaxSize) {
+  // По рекомендации пиров тестировать эту функцию не стоит
+  //
+  // std::set<int> trueSet1;
+  // s21::Set<int> testSet1;
+  // EXPECT_EQ(testSet1.max_size(), trueSet1.max_size());
+  // std::set<char> trueSet2;
+  // s21::Set<char> testSet2;
+  // EXPECT_EQ(testSet2.max_size(), trueSet2.max_size());
+  // testSet1.clear();
+  // testSet2.clear();
+}
+
+TEST(SetSuite, Clear) {
+  s21::Set<int> testSet({50, 45, 1, 30});
+  s21::Set<int> trueSet;
+  EXPECT_EQ(testSet.size(), 4);
+  testSet.clear();
+  EXPECT_EQ(testSet, trueSet);
+  testSet.clear();
+  trueSet.clear();
+}
+
+TEST(SetSuite, Insert) {
+  s21::Set<int> testSet1({50, 45, 1});
+  s21::Set<int> trueSet1({50, 45, 1, 30});
+  s21::Set<int> testSet2;
+  s21::Set<int> trueSet2({30});
+  s21::Set<int> testSet3({30});
+  s21::Set<int> trueSet3({30});
+  EXPECT_TRUE(testSet1.insert(30).second);
+  EXPECT_TRUE(testSet2.insert(30).second);
+  EXPECT_FALSE(testSet3.insert(30).second);
+  EXPECT_EQ(testSet1.size(), trueSet1.size());
+  EXPECT_EQ(testSet2.size(), trueSet2.size());
+  EXPECT_EQ(testSet3.size(), trueSet3.size());
+  testSet1.clear();
+  testSet2.clear();
+  testSet3.clear();
+  trueSet1.clear();
+  trueSet2.clear();
+  trueSet3.clear();
+}
+
+TEST(SetSuite, Erase) {
+  s21::Set<int> testSet1({50, 45, 1, 30});
+  s21::Set<int> trueSet1({50, 45, 1});
+  s21::Set<int> testSet2({30});
+  s21::Set<int> trueSet2;
+  s21::Set<int> testSet3({1, 2, 3});
+  s21::Set<int> trueSet3({1, 2, 3});
+  testSet1.erase(testSet1.find(30));
+  testSet2.erase(testSet2.find(30));
+  EXPECT_ANY_THROW(testSet3.erase(testSet3.find(30)));
+  EXPECT_EQ(testSet1.size(), trueSet1.size());
+  EXPECT_EQ(testSet2.size(), trueSet2.size());
+  EXPECT_EQ(testSet3.size(), trueSet3.size());
+  testSet1.clear();
+  testSet2.clear();
+  testSet3.clear();
+  trueSet1.clear();
+  trueSet2.clear();
+  trueSet3.clear();
+}
+
+TEST(SetSuite, Find) {
+  s21::Set<int> testSet1({50, 45, 1, 30});
+  std::set<int> trueSet1({50, 45, 1, 30});
+  s21::Set<int> testSet2;
+  EXPECT_EQ(*testSet1.find(30), *trueSet1.find(30));
+  EXPECT_ANY_THROW(*testSet1.find(-1));
+  EXPECT_ANY_THROW(*testSet2.find(30));
+  testSet1.clear();
+  trueSet1.clear();
+  testSet2.clear();
+}
+
+TEST(SetSuite, Contains) {
+  s21::Set<int> testSet1({50, 45, 1, 30});
+  s21::Set<int> testSet2;
+  EXPECT_TRUE(testSet1.contains(1));
+  EXPECT_TRUE(testSet1.contains(45));
+  EXPECT_TRUE(testSet1.contains(50));
+  EXPECT_TRUE(testSet1.contains(30));
+  EXPECT_FALSE(testSet1.contains(-1));
+  EXPECT_FALSE(testSet2.contains(30));
+  testSet1.clear();
+  testSet2.clear();
+}
+
+TEST(SetSuite, Merge) {
+  s21::Set<int> testSet11({50, 45, 1, 30});
+  s21::Set<int> testSet12({49, 46, 55, 51, 53});
+  s21::Set<int> trueSet1({50, 45, 1, 30, 49, 46, 55, 51, 53});
+  s21::Set<int> testSet21({50, 45, 1, 30});
+  s21::Set<int> testSet22;
+  s21::Set<int> trueSet2({50, 45, 1, 30});
+  testSet11.merge(testSet12);
+  testSet21.merge(testSet22);
+  EXPECT_EQ(testSet11, trueSet1);
+  EXPECT_EQ(testSet21, trueSet2);
+  testSet11.clear();
+  testSet12.clear();
+  trueSet1.clear();
+  testSet21.clear();
+  testSet22.clear();
+  trueSet2.clear();
 }
